6e01741466e146e6df29f171142120f9443d936b
rajugupta0 <raju200115@gmail.com>
2024-01-30 12:05:35 +0530

    added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './App.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
    <>
    hello
+   <Footer />
    </>
   )
 }
